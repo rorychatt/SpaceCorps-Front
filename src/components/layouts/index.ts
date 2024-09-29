862d391bbb9eb399eb304325bd2064f24abe98b1
rorychatt <pavel.rinne@gmail.com>
2024-09-29 22:28:53 +0200

    feat: daisyui installed

---
+++ src/components/layouts/index.ts
@@ -0,0 +1,3 @@
+export * from './nav'
+export * from './main'
+export * from './footer'
---
+++ src/components/layouts/nav.tsx
@@ -0,0 +1,11 @@
+export function Nav() {
+    return (
+        <nav className="nav">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
