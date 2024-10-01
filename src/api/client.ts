2024e98b5cfb1cc810c28b346295bef1b778db2a
rorychatt <pavel.rinne@gmail.com>
2024-10-01 11:38:45 +0200

    feat: schema generation script

---
+++ src/api/client.ts
@@ -0,0 +1,8 @@
+import createClient from "openapi-fetch";
+import { paths } from "./schema";
+
+export const client = createClient<paths>(
+    {
+        baseUrl: import.meta.env.VITE_API_HOST,
+    }
+);
