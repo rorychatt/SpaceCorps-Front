bb38bf514a61ae069c0378bc477ff2d4e15fe0fd
rorychatt <pavel.rinne@gmail.com>
2024-10-26 23:35:10 +0200

    feat: create login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,12 @@ export const routes: Routes = [{
     const module = await import('./home/home.component');
     return module.HomeComponent;
   },
-},];
+},
+  {
+    path: 'login',
+    loadComponent: async () => {
+      const module = await import('./login/login.component');
+      return module.LoginComponent;
+    }
+  },
+];
