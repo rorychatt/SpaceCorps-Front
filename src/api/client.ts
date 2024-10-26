d223508778bec0aed172d908c9cc61679573fbae
rorychatt <pavel.rinne@gmail.com>
2024-10-26 23:12:27 +0200

    feat: create main page

--- src/api/client.ts
+++
@@ -1,8 +0,0 @@
-import createClient from "openapi-fetch";
-import { paths } from "./schema";
-
-export const client = createClient<paths>(
-    {
-        baseUrl: import.meta.env.VITE_API_HOST,
-    }
-);
