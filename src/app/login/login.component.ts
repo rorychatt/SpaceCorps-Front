99d7d9de3f26436579ce26c5c4a9b2e55c0f5d8c
rorychatt <pavel.rinne@gmail.com>
2024-10-27 16:58:57 +0100

    feat: template for login form new angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,49 +1,24 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    NgIf
+    NgClass
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  })
 
-  loginForm: FormGroup;
-  isRegistering = false;
-
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['']
-    }, { validators: this.passwordMatchValidator });
-  }
-
-  passwordMatchValidator(form: FormGroup) {
-    return form.get('password')?.value === form.get('confirmPassword')?.value
-      ? null : { mismatch: true };
-  }
-
-  toggleMode() {
-    this.isRegistering = !this.isRegistering;
-    if (!this.isRegistering) {
-      this.loginForm.get('confirmPassword')?.reset();
-    }
-  }
-
-  onSubmit() {
-    if (this.loginForm.valid) {
-      if (this.isRegistering) {
-        // Handle registration logic
-      } else {
-        // Handle login logic
-      }
-    }
+  handleLogin() {
+    console.log(this.loginForm.value);
   }
 }
