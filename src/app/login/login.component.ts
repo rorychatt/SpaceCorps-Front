81a17b11587cbfdacef5623002a6d4a7aa3b2eee
rorychatt <pavel.rinne@gmail.com>
2024-10-27 17:29:13 +0100

    feat: use new login stuff

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../components/login-form/login-form.component';
+import { RegisterFormComponent } from '../components/register-form/register-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    LoginFormComponent
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
 }
