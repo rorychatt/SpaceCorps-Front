2bcf03646c56c8bdc449ad88ffe726438b3a08bc
rorychatt <pavel.rinne@gmail.com>
2024-10-27 17:24:42 +0100

    feat: use new forms in main login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,15 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { NgClass } from '@angular/common';
+import { LoginFormComponent } from '../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    ReactiveFormsModule,
-    NgClass
+    LoginFormComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
-  })
 
-  handleLogin() {
-    console.log(this.loginForm.value);
-  }
 }
