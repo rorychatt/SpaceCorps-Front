08763f8ee747924b2b461b96c560e23bb558f3e5
rorychatt <pavel.rinne@gmail.com>
2024-11-19 22:25:19 +0100

    feat: update dto

--- src/app/models/entity/SpawnableAlien.ts
+++ src/app/models/entity/SpawnableAlien.ts
@@ -1,4 +1,5 @@
 export type SpawnableAlien = {
+  id: number;
   name: string;
   spawnLimit: number;
 }
--- src/app/models/entity/StaticEntity.ts
+++ src/app/models/entity/StaticEntity.ts
@@ -1,3 +1,12 @@
 export type StaticEntity = {
   name: string;
+  position: {
+    x: number;
+    y: number;
+    z: number;
+  }
+  id: number;
+  locationName: string;
+  safeZoneRadii: number;
+  destination: string;
 }
