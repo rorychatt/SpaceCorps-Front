3408ed1f12c1ca5c3a233c7b44d3a54d4cd08576
rorychatt <pavel.rinne@gmail.com>
2024-11-20 21:37:10 +0100

    feat: display online status next to avatar

--- src/app/components/player-info-general-info/player-info-general-info.component.ts
+++ src/app/components/player-info-general-info/player-info-general-info.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgClass } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-player-info-general-info',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    AsyncPipe
+  ],
   templateUrl: './player-info-general-info.component.html',
   styleUrl: './player-info-general-info.component.scss'
 })
 export class PlayerInfoGeneralInfoComponent {
+
+  authService = inject(AuthService)
 
 }
