ce53bda416870006168b9dacb54ab5e38a131988
dmitri.belov.db <dmitri.belov.db@gmail.com>
2024-11-20 22:14:30 +0200

    General Info Containter

---
+++ src/app/components/player-info-general-info/player-info-general-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-info-general-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './player-info-general-info.component.html',
+  styleUrl: './player-info-general-info.component.scss'
+})
+export class PlayerInfoGeneralInfoComponent {
+
+}
