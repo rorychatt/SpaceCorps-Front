5bd077dd529b497257a1d272a99d26163915c66e
rorychatt <pavel.rinne@gmail.com>
2024-12-14 15:15:57 +0100

    feat: add default thrusters

---
+++ src/app/models/dataEntries/itemEntries/itemEntryGenScripts/defaultThrusters.ts
@@ -0,0 +1,13 @@
+import { ThrusterEntity } from '../ThrusterEntity';
+
+export const defaultThrusters: ThrusterEntity[] = [
+  {
+    name: "Thruster 1",
+    itemType: "ThrusterItem",
+    id: 1,
+    addBaseSpeed: 5,
+    baseSpeedMultiplier: 1,
+    priceCats: 6666,
+    priceThulium: 0
+  }
+]
