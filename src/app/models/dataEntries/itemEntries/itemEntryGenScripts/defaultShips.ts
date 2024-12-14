e1c4d8d1e2b939ea130381123872c9be28a0b9c5
rorychatt <pavel.rinne@gmail.com>
2024-12-14 15:13:58 +0100

    feat: add default ships

---
+++ src/app/models/dataEntries/itemEntries/itemEntryGenScripts/defaultShips.ts
@@ -0,0 +1,88 @@
+import { ShipEntry } from '../ShipEntry';
+
+export const defaultShips: ShipEntry[] = [
+  {
+    name: "Protos",
+    itemType: "ShipItem",
+    id: 1,
+    baseHealth: 8000,
+    baseSpeed: 150,
+    engineSlots: 2,
+    shieldSlots: 2,
+    laserSlots: 2,
+    priceCats: 1111,
+    priceThulium: 0
+  },
+  {
+    name: "Ostrion",
+    itemType: "ShipItem",
+    id: 2,
+    baseHealth: 32000,
+    baseSpeed: 180,
+    engineSlots: 4,
+    shieldSlots: 3,
+    laserSlots: 3,
+    priceCats: 155000,
+    priceThulium: 0
+  },
+  {
+    name: "Paragon",
+    itemType: "ShipItem",
+    id: 3,
+    baseHealth: 96000,
+    baseSpeed: 200,
+    engineSlots: 5,
+    shieldSlots: 5,
+    laserSlots: 5,
+    priceCats: 655000,
+    priceThulium: 0
+  },
+  {
+    name: "Echelon",
+    itemType: "ShipItem",
+    id: 4,
+    baseHealth: 128000,
+    baseSpeed: 210,
+    engineSlots: 6,
+    shieldSlots: 6,
+    laserSlots: 6,
+    priceCats: 0,
+    priceThulium: 12000
+  },
+  {
+    name: "Specter",
+    itemType: "ShipItem",
+    id: 5,
+    baseHealth: 196000,
+    baseSpeed: 185,
+    engineSlots: 8,
+    shieldSlots: 6,
+    laserSlots: 6,
+    priceCats: 1000000,
+    priceThulium: 0
+  },
+  {
+    name: "Orionix",
+    itemType: "ShipItem",
+    id: 6,
+    baseHealth: 256000,
+    baseSpeed: 225,
+    engineSlots: 12,
+    shieldSlots: 10,
+    laserSlots: 10,
+    priceCats: 0,
+    priceThulium: 25000
+  },
+  {
+    name: "Wraith",
+    itemType: "ShipItem",
+    id: 7,
+    baseHealth: 324000,
+    baseSpeed: 210,
+    engineSlots: 14,
+    shieldSlots: 14,
+    laserSlots: 14,
+    priceCats: 0,
+    priceThulium: 65000
+  }
+];
