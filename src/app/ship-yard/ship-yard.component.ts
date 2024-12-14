0ea2dea392f3c7339472c7104f0c30135f057296
rorychatt <pavel.rinne@gmail.com>
2024-12-14 16:19:51 +0100

    feat: highlight selected category

--- src/app/ship-yard/ship-yard.component.ts
+++ src/app/ship-yard/ship-yard.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForOf, NgIf } from '@angular/common';
+import { ApiService } from '../services/api.service';
+
+interface Item {
+  name: string;
+  category: string;
+  priceCats: number;
+  priceThulium: number;
+  icon: string;
+}
 
 @Component({
   selector: 'app-ship-yard',
-  standalone: true,
-  imports: [],
   templateUrl: './ship-yard.component.html',
-  styleUrl: './ship-yard.component.scss'
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgIf
+  ],
+  styleUrls: ['./ship-yard.component.scss']
 })
-export class ShipYardComponent {
 
+export class ShipYardComponent implements OnInit {
+  categories: string[] = ['Ships', 'Lasers', 'Laser Amps', 'Shields', 'Shield Cells', 'Engines', 'Engine Thrusters', 'Laser Ammo'];
+  selectedCategory: string | null = null;
+  items: SellableItem[] = [];
+  playerBalance = { cats: 0, thulium: 0 };
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.fetchPlayerBalance();
+  }
+
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+    this.fetchItems(category);
+  }
+
+  fetchItems(category: string) {
+    const categoryKey = ItemTypeDictionary[category as keyof typeof ItemTypeDictionary];
+    this.apiService.getItemEntriesByCategory(categoryKey).subscribe(data => {
+      this.items = data as SellableItem[];
+    });
+  }
+
+  fetchPlayerBalance() {
+
+  }
+
+}
+
+type SellableItem = {
+  name: string,
+  priceCats: number,
+  priceThulium: number,
+  category: string,
+}
+
+const ItemTypeDictionary = {
+  "Ships": "ShipEntries",
+  "Lasers": "LaserEntries",
+  "Laser Amps": "LaserAmpEntries",
+  "Shields": "ShieldEntries",
+  "Shield Cells": "ShieldCellEntries",
+  "Engines": "EngineEntries",
+  "Engine Thrusters": "ThrusterEntries",
+  "Laser Ammo": "LaserAmmoEntries"
 }
