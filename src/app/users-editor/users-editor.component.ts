e6fe2391eb3270a16d944cc5c6a8d7cf60ea04da
rorychatt <pavel.rinne@gmail.com>
2024-12-29 19:12:35 +0200

    feat: init users editor component

---
+++ src/app/users-editor/users-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-editor.component.html',
+  styleUrl: './users-editor.component.scss'
+})
+export class UsersEditorComponent {
+
+}
