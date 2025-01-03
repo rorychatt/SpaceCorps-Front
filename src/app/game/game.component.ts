40750be4810b8ba6ef28e14c37dd970de2fb1e51
rorychatt <pavel.rinne@gmail.com>
2025-01-03 19:40:00 +0200

    feat: add skyboxes

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,8 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import * as THREE from 'three';
 import { HubService } from '../services/hub.service';
 import { ActivatedRoute } from '@angular/router';
+import { SpaceMapData } from './types/SpaceMapData';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 @Component({
   selector: 'app-game',
@@ -14,6 +16,8 @@ export class GameComponent implements OnInit {
 
   private camera?: THREE.PerspectiveCamera;
   private renderer?: THREE.WebGLRenderer;
+  private scene?: THREE.Scene;
+  private controls?: OrbitControls
 
   constructor (private hubService: HubService, private route: ActivatedRoute) {
   }
@@ -28,7 +32,7 @@ export class GameComponent implements OnInit {
   }
 
   private initializeThreeJs (): void {
-    const scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -38,21 +42,26 @@ export class GameComponent implements OnInit {
 
     document.body.appendChild(this.renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    this.controls = new OrbitControls(
+      this.camera!,
+      this.renderer!.domElement
+    )
+
+    this.controls.enableDamping = true;
 
     this.camera.position.z = 5;
 
     const animate = () => {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      this.renderer!.render(scene, this.camera!);
+      this.renderer!.render(this.scene!, this.camera!);
     };
 
     animate();
+
+    void this.loadNewSpaceMap({
+      mapname: 'G-1',
+      id: "1"
+    })
   }
 
   @HostListener('window:resize', ['$event'])
@@ -66,5 +75,64 @@ export class GameComponent implements OnInit {
     this.hubService.on('server-side-log', (data) => {
       console.log('Received data:', data);
     });
+  }
+
+  private async loadNewSpaceMap (spaceMapData: SpaceMapData) {
+    await this.clearScene();
+    await this.loadMapEnvironment(spaceMapData);
+  }
+
+  private async clearScene () {
+  }
+
+  private async loadMapEnvironment (spaceMapData: SpaceMapData) {
+    await this.createStars();
+    await this.createLighting();
+    await this.createSkybox(spaceMapData.mapname);
+    await this.createStaticEntities();
+  }
+
+  private async createStars () {
+
+  }
+
+  private async createLighting () {
+  }
+
+  private async createSkybox (mapname?: string) {
+    if (!this.scene) {
+      console.error('Scene not initialized');
+      return;
+    }
+
+    const loader = new THREE.CubeTextureLoader();
+    const defaultMapname = 'G-1';
+
+    if (!mapname) {
+      mapname = defaultMapname;
+      console.log(`No mapname provided, loading default map ${defaultMapname}`);
+    }
+
+    const loadSkybox = (name: string) => {
+      return loader.load([
+        `./spacemaps/${name}/right.png`,
+        `./spacemaps/${name}/left.png`,
+        `./spacemaps/${name}/top.png`,
+        `./spacemaps/${name}/bottom.png`,
+        `./spacemaps/${name}/front.png`,
+        `./spacemaps/${name}/back.png`,
+      ]);
+    };
+
+    try {
+      this.scene.background = loadSkybox(mapname);
+    } catch (error) {
+      console.error(`Failed to load skybox for ${mapname}, loading default skybox`, error);
+      this.scene.background = loadSkybox(defaultMapname);
+    }
+  }
+
+  private async createStaticEntities () {
+
   }
 }
