3cbbcc9308c503013c7f6b924b62196e7892f426
rorychatt <pavel.rinne@gmail.com>
2025-01-19 23:52:58 +0100

    fix: solve async comparisons

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,42 +1,38 @@
-import { Component, inject } from '@angular/core';
-import { version, reqBackendVersion } from '../../../../package.json';
-import { ApiService } from "../../services/api.service";
+import {Component, inject} from '@angular/core';
+import {version, reqBackendVersion} from '../../../../package.json';
+import {ApiService} from "../../services/api.service";
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss'
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
-    clientVersion: string = version;
-    outDated: boolean = false;
-    serverVersion: string | null = null;
+  clientVersion: string = version;
+  outDated: boolean = false;
+  serverVersion: string | null = null;
 
-    apiService = inject(ApiService)
+  apiService = inject(ApiService)
 
-    ngOnInit() {
-        this.apiService.getBackendVersion().subscribe({
-            next: (serverInfo) => {
-                this.serverVersion = serverInfo.version
-            },
-            error: (error) => {
-                console.error(error)
-            }
-        });
+  ngOnInit() {
+    this.apiService.getBackendVersion().subscribe({
+      next: (serverInfo) => {
+        this.serverVersion = serverInfo.version
 
-        if (this.serverVersion) {
-            if (
-                this.getMinorVersion(this.serverVersion) >
-                this.getMinorVersion(reqBackendVersion)
-            ) {
-                this.outDated = true;
-            }
-        }
+        if (!this.serverVersion) return;
 
-    }
+        this.outDated = this.getMinorVersion(this.serverVersion)
+          > this.getMinorVersion(reqBackendVersion);
+      },
+      error: (error) => {
+        console.error(error)
+      }
+    });
+  }
 
-    private getMinorVersion(fullVersion: string): number {
-        return parseInt(fullVersion.split('.')[1])
-    }
+  private getMinorVersion(fullVersion: string): number {
+    return parseInt(fullVersion.split('.')[1])
+  }
+
 }
