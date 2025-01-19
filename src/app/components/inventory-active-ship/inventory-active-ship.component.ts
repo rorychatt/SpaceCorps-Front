8308bf853ee4607c0e3166e458cd8f09c9cb3312
rorychatt <pavel.rinne@gmail.com>
2025-01-19 16:43:08 +0100

    feat: init inventory active ship component

---
+++ src/app/components/inventory-active-ship/inventory-active-ship.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {ShipModelComponent} from '../ship-model/ship-model.component';
+
+@Component({
+  selector: 'app-inventory-active-ship',
+  imports: [
+    ShipModelComponent
+  ],
+  templateUrl: './inventory-active-ship.component.html',
+  styleUrl: './inventory-active-ship.component.scss'
+})
+export class InventoryActiveShipComponent {
+
+}
