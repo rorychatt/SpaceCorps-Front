714730c72e83002f9cd44bd703222498c015a79f
rorychatt <pavel.rinne@gmail.com>
2025-01-26 01:06:16 +0100

    feat: add basic sorting to items by name and type

--- src/app/components/inventory-all-items/inventory-all-items.component.ts
+++ src/app/components/inventory-all-items/inventory-all-items.component.ts
@@ -11,4 +11,21 @@ export class InventoryAllItemsComponent {
   @Input({
     required: true
   }) items: InventoryItem[] | undefined;
+
+  ngOnInit() {
+
+    if (!this.items) {
+      console.warn('No items were provided to the InventoryAllItemsComponent');
+      this.items = [];
+    }
+
+    // sort items by item type and then by item name
+    this.items = this.items.sort((a, b) => {
+      if (a.itemType === b.itemType) {
+        return a.name.localeCompare(b.name);
+      } else {
+        return a.itemType.localeCompare(b.itemType);
+      }
+    });
+  }
 }
