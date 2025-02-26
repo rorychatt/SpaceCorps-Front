691f4af3c0489feab85b3fbe94b66c26195551e8
rorychatt <pavel.rinne@gmail.com>
2025-02-26 17:57:23 +0100

    feat: add support for styles

---
+++ src/app/components/theme-picker/theme-picker.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-theme-picker',
+  imports: [],
+  templateUrl: './theme-picker.component.html',
+  styleUrl: './theme-picker.component.scss'
+})
+export class ThemePickerComponent implements OnInit {
+  themes = [
+    "dark",
+    "light",
+    "black",
+    "dracula",
+    "night",
+    "sunset",
+    "business"
+  ]
+
+  selectedTheme = "dark";
+
+
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('selectedTheme');
+    if(savedTheme) {
+      this.selectedTheme = savedTheme;
+      this.applyTheme(savedTheme);
+    } else {
+      this.applyTheme(this.selectedTheme);
+    }
+  }
+
+  onThemeChange(event: Event) {
+    const theme = (event.target as HTMLInputElement).value;
+    this.selectedTheme = theme;
+    localStorage.setItem('selectedTheme', theme);
+    this.applyTheme(theme);
+  }
+
+  applyTheme(theme: string) {
+    document.documentElement.setAttribute('data-theme', theme);
+  }
+}
